4cd0c65cd4b5656f69c5d57733d36db9fd937f28
shrihannaruvadi <shrihan.naruvadi@gmail.com>
2025-06-20 15:59:35 -0500

    added structure files and src folders

---
+++ src/__init__.py
@@ -0,0 +1,17 @@
+"""
+Stock Price Prediction Project
+==============================
+
+This package contains modules for stock price prediction using machine learning.
+
+
+Modules: 
+-------
+- data_ingestion: Functions for downloading and processing stock data.
+- feature_engineering: Functions for creating predictive features.
+- models: Various machine learning models for stock price predictions.
+- main: FastAPI application for serving purposes
+
+"""
+__version__ = "0.0.1"
+__name__ = "Shrihan Naruvadi"
---
+++ src/main.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import numpy as np
+import yfinance as yf
+import sklearn
+import torch
+import matplotlib
+
+if __name__ == "main":
+    print("Hello World")
